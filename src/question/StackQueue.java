09941b53d4174c832e59f72521dcb404df645e87
king <bjlajin@163.com>
2019-09-25 15:20:05 +0800

    栈实现队列

---
+++ src/question/StackQueue.java
@@ -0,0 +1,54 @@
+package question;
+
+import coding.StackArray;
+
+/**
+ * 用栈实现一个队列
+ */
+public class StackQueue {
+    private StackArray<Integer> stackA = new StackArray<Integer>();
+    private StackArray<Integer> stackB = new StackArray<Integer>();
+
+    /**
+     * 入队操作
+     *
+     * @param element 入队的元素
+     */
+    public void enQueue(int element) {
+        stackA.push(element);
+    }
+
+    /**
+     * 出队操作
+     */
+    public Integer deQueue() {
+        if (stackB.isEmpty()) {
+            if (stackA.isEmpty()) {
+                return null;
+            }
+            transfer();
+        }
+        return stackB.pop();
+    }
+
+    /**
+     * 栈A元素转移到栈B
+     */
+    private void transfer() {
+        while (!stackA.isEmpty()) {
+            stackB.push(stackA.pop());
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        StackQueue stackQueue = new StackQueue();
+        stackQueue.enQueue(1);
+        stackQueue.enQueue(2);
+        stackQueue.enQueue(3);
+        System.out.println(stackQueue.deQueue());
+        System.out.println(stackQueue.deQueue());
+        stackQueue.enQueue(4);
+        System.out.println(stackQueue.deQueue());
+        System.out.println(stackQueue.deQueue());
+    }
+}
